346c892ae71452119679c23b2c0ba13a2d35b9d8
takeru-a <pypy20191022@gmail.com>
2021-10-12 18:32:27 +0900

    セキュリティ機能の追加とオブジェクトの共有

--- src/main/java/oit/is/z1017/kaizi/janken/controller/Lec02Controller.java
+++ src/main/java/oit/is/z1017/kaizi/janken/controller/Lec02Controller.java
@@ -1,4 +1,8 @@
 package oit.is.z1017.kaizi.janken.controller;
+
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -7,11 +11,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import oit.is.z1017.kaizi.janken.model.Janken;
+import oit.is.z1017.kaizi.janken.model.Entry;
 
 @Controller
-
-
 public class Lec02Controller{
+  @Autowired
+  private Entry entry;
 
  @GetMapping("/lec02janken")
   public String lec02janken(@RequestParam String hand, ModelMap model) {
@@ -24,14 +29,16 @@ public class Lec02Controller{
   model.addAttribute("result", janken.getResult());
   return "lec02.html";
   }
- @GetMapping("/lec02")
-  public String lec02() {
+
+@GetMapping("/lec02")
+  public String lec02(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    this.entry.addUser(loginUser);
+    model.addAttribute("room", this.entry);
+    model.addAttribute("login_user", loginUser);
+
     return "lec02.html";
   }
- @PostMapping("/lec02")
-  public String lec02(@RequestParam String name, ModelMap model) {
-    String username = name;
-    model.addAttribute("username", username);
-    return "lec02.html";
-  }
+
+
 }
---
+++ src/main/java/oit/is/z1017/kaizi/janken/security/Lec03AuthConfiguration.java
@@ -0,0 +1,38 @@
+package oit.is.z1017.kaizi.janken.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class Lec03AuthConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication().withUser("e1b19027").password(passwordEncoder().encode("b@n@n@")).roles("USER");
+
+    // $ sshrun htpasswd -nbBC 10 master 1192
+    // pass: 1192
+    auth.inMemoryAuthentication().withUser("master")
+        .password("$2y$10$WtUytKKWrPRVlFUhG4xWt.Gu3EDeiTDTpwEsutfns67jt4NbIu2D2").roles("ADMIN");
+  }
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    // Spring Securityのフォームを利用してログインを行う
+    http.formLogin();
+    http.authorizeRequests().antMatchers("/lec02/**").authenticated();
+    // Spring Securityの機能を利用してログアウト．ログアウト時は http://localhost:8080/ に戻る
+    http.logout().logoutSuccessUrl("/");
+  }
+}
